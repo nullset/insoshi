ef4c53084ebb9437ea8b0124b886e5a3bad17ba4
Nathan Wright <nathan@brandalism.com>
2009-08-03 03:17:32 -0600

    Set password remider email to deployer

--- app/models/person_mailer.rb
+++ app/models/person_mailer.rb
@@ -10,7 +10,7 @@ class PersonMailer < ActionMailer::Base
   end
   
   def password_reminder(person)
-    from         "Password reminder <password-reminder@#{domain}>"
+    from         "Password reminder <deployer@#{domain}>"
     recipients   person.email
     subject      formatted_subject("Password reminder")
     body         "person" => person
