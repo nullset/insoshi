96519fdf877de0565bf16651519e084d09ebd20c
Nathan Wright <nathan@brandalism.com>
2009-08-11 22:35:14 -0600

    Switched to sendmail, see if this works.

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -2,11 +2,18 @@ begin
   unless test?
     global_prefs = Preference.find(:first)
     if global_prefs.email_notifications?
-      ActionMailer::Base.delivery_method = :smtp
-      ActionMailer::Base.smtp_settings = {
-        :address    => global_prefs.smtp_server,
-        :port       => 25,
-        :domain     => global_prefs.domain
+      # ActionMailer::Base.delivery_method = :smtp
+      # ActionMailer::Base.smtp_settings = {
+      #   :address    => global_prefs.smtp_server,
+      #   :port       => 25,
+      #   :domain     => global_prefs.domain
+      # }
+      
+      ActionMailer::Base.delivery_method = :sendmail
+
+      ActionMailer::Base.sendmail_settings = {
+      :location       => '/usr/sbin/sendmail',
+      :arguments      => '-i -t'
       }
     end
   end
